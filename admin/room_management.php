00d349e28cbb213363e40a185e595fac9d5489d7
jbbertt <31472515+jbbertt@users.noreply.github.com>
2017-11-04 22:47:09 +0800

    admin functions

---
+++ admin/room_management.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<title>Admin</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+
+
+  <div class="w3-container w3-teal">
+  <h1>Paglaom Hotel</h1>
+
+</div>
+<!-- Sidebar -->
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:15%">
+  <h3 class="w3-bar-item">Menu</h3>
+  <a href="home.php" class="w3-bar-item w3-button"><img src="img/home.png" height="35  px" width="50px">Home</a>
+   <a href="users.php" class="w3-bar-item w3-button"><img src="img/users.png" height="35  px" width="50px">  Users</a>
+  <a href="room_management.php" class="w3-bar-item w3-button"><img src="img/room.png" height="35  px" width="50px">      Room</a>
+  <a href="booking.php" class="w3-bar-item w3-button"><img src="img/booking.png" height="35  px" width="50px">    Booking</a>
+</div>
+
+
+
+<!-- Page Content -->
+<div style="margin-left:15%">
+<div class="w3-container">
+<h2>Room Management</h2>
+
+<p>The sidebar with is set with "style="width:25%".</p>
+<p>The left margin of the page content is set to the same value.</p>
+</div>
+
+</div>
+      
+</body>
+</html>
---
+++ public/register.php
@@ -0,0 +1,81 @@
+<?php 
+
+require_once 'config.php';
+
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<div class="header">
+		<h2>Register</h2>
+	</div>
+
+	<div class="container">
+		
+
+		<div class="body">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+				
+					<div class="input-group">
+					<label>Name<sup>*</sup></label>
+					<input type="text" name="name">
+					</div>
+
+					<div class="input-group">
+					<label>Email<sup>*</sup></label>
+					<input type="text" name="email">
+					</div>
+
+					<div class="input-group">
+					<label>Mobile Number</label>
+					<input type="text" name="mobile_number">
+					</div>
+
+					<div class="input-group ">
+					<label>Name<sup>*</sup></label>
+					<input type="text" name="name">
+					</div>
+
+					<div class="input-group">
+					<label>Address</label>
+					<input type="text" name="address">
+					</div>
+
+					<div class="input-group">
+					<label>Username<sup>*</sup></label>
+					<input type="text" name="ussername">
+					</div>
+
+					<div class="input-group">
+					<label>Password<sup>*</sup></label>
+					<input type="text" name="password1">
+					</div>
+
+					<div class="input-group">
+					<label>Confirm Password</label>
+					<input type="text" name="password">
+					</div>
+
+					<div class="input-group">
+					<button type="submit" class="btn" name="reg_user">Register</button>
+					</div>
+
+				<p>
+				Already a member? <a href="login.php">Sign in</a>
+			   </p>
+			</form>
+		
+	</div>
+
+
+</body>
+</html>
